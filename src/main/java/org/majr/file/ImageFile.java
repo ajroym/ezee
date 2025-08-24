1d70fe8f86f695278bce0b5769e04ed966a35496
AJ <ajroym00@gmail.com>
2025-08-24 17:39:38 -0400

    Fixed issues with checking if a file exists

--- src/main/java/org/majr/file/ImageFile.java
+++ src/main/java/org/majr/file/ImageFile.java
@@ -2,34 +2,35 @@ package org.majr.file;
 
 import org.majr.exceptions.InvalidFormatException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ImageFile {
 
+    // Update to handle different types of Path Structures, as well as ensure
+    // the desired path exists and is in a correct path format.
     public static byte[] extract(String pathStr) throws IOException {
-        try {
-            if (!isValidImageFormat(pathStr.substring(pathStr.length() - 5))) {
-                throw new InvalidFormatException("Invalid format requested at " +
-                                                pathStr +
-                                                "\n\nPlease enter a file of one of the following formats: .bpm, .png");
-            }
-            return Files.readAllBytes(Path.of(pathStr));
-
-        } catch(OutOfMemoryError memError) {
-            System.out.println();
-
-            return null;
-        } catch (InvalidFormatException formatError) {
-            System.out.println(formatError.getMessage());
-
-            return null;
+        Path path = Path.of(pathStr);
+        if (!doesPathExist(path)) {
+            throw new FileNotFoundException();
         }
+
+        if (!isValidImageFormat(pathStr.substring(pathStr.length() - 4))) {
+            throw new InvalidFormatException("Invalid format requested at " +
+                                            pathStr +
+                                            "\n\nPlease enter a file of one of the following formats: .bpm, .png");
+        }
+        return Files.readAllBytes(path);
     }
 
     private static boolean isValidImageFormat(String fileExtension) {
         return fileExtension.equals(".bmp") ||
                 fileExtension.equals(".png");
+    }
+
+    private static boolean doesPathExist(Path path) {
+        return path != null && Files.exists(path);
     }
 }
