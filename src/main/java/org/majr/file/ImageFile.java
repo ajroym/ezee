c78742b277c4b15478113a6f4c102889fa76b9af
AJ <ajroym00@gmail.com>
2025-08-24 15:49:37 -0400

    Developed method to extract bytes from file

--- src/main/java/org/majr/file/ImageFile.java
+++ src/main/java/org/majr/file/ImageFile.java
@@ -1,16 +1,35 @@
 package org.majr.file;
 
+import org.majr.exceptions.InvalidFormatException;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ImageFile {
 
-    private final Path path;
+    public static byte[] extract(String pathStr) throws IOException {
+        try {
+            if (!isValidImageFormat(pathStr.substring(pathStr.length() - 5))) {
+                throw new InvalidFormatException("Invalid format requested at " +
+                                                pathStr +
+                                                "\n\nPlease enter a file of one of the following formats: .bpm, .png");
+            }
+            return Files.readAllBytes(Path.of(pathStr));
 
-    public ImageFile(String path) {
-        this.path = Path.of(path);
+        } catch(OutOfMemoryError memError) {
+            System.out.println();
+
+            return null;
+        } catch (InvalidFormatException formatError) {
+            System.out.println(formatError.getMessage());
+
+            return null;
+        }
     }
 
-    public byte[] extract() {
-        return null;
+    private static boolean isValidImageFormat(String fileExtension) {
+        return fileExtension.equals(".bmp") ||
+                fileExtension.equals(".png");
     }
 }
