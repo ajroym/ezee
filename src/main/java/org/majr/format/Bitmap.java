17f76aacae073c26c71aaf9270558c712609294f
AJ <ajroym00@gmail.com>
2025-08-24 14:50:48 -0400

    Added basic Bitmap class structure

--- src/main/java/org/majr/format/Bitmap.java
+++ src/main/java/org/majr/format/Bitmap.java
@@ -1,4 +1,23 @@
 package org.majr.format;
 
 public class Bitmap {
+
+    private int WIDTH;
+
+    private int HEIGHT;
+
+    private int COMPRESSION_METHOD;
+
+    private short COLOR_DEPTH;
+
+    private final short COLOR_PLANES = 1;
+
+    private final byte[] PIXEL_ARRAY;
+
+    private final byte[] COLOR_TABLE;
+
+    public Bitmap() {
+        this.PIXEL_ARRAY = new byte[] {};
+        this.COLOR_TABLE = new byte[] {};
+    }
 }
